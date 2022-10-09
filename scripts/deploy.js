fe5f5312188e25df1dd150ab5efa14b5f73f1e6f
petya0111 <pmarinova0111@gmail.com>
2022-10-09 17:40:21 +0300

    prettier config

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,25 @@
-const hre = require('hardhat')
-const ethers = hre.ethers;
+const hre = require("hardhat")
+const ethers = hre.ethers
 
 async function deployLibraryContract() {
-    await hre.run('compile'); // We are compiling the contracts using subtask
-    const [deployer] = await ethers.getSigners(); // We are getting the deployer
+    await hre.run("compile") // We are compiling the contracts using subtask
+    const [deployer] = await ethers.getSigners() // We are getting the deployer
 
-    console.log('Deploying contracts with the account:', deployer.address); // We are printing the address of the deployer
-    console.log('Account balance:', (await deployer.getBalance()).toString()); // We are printing the account balance
+    console.log("Deploying contracts with the account:", deployer.address) // We are printing the address of the deployer
+    console.log("Account balance:", (await deployer.getBalance()).toString()) // We are printing the account balance
 
-    const bookLibrary = await ethers.getContractFactory("Library"); // Get the contract factory with the signer from the wallet created
-    const bookLibraryContract = await bookLibrary.deploy();
-    console.log('Waiting for bookLibrary deployment...');
-    await bookLibraryContract.deployed();
+    const bookLibrary = await ethers.getContractFactory("Library") // Get the contract factory with the signer from the wallet created
+    const bookLibraryContract = await bookLibrary.deploy()
+    console.log("Waiting for bookLibrary deployment...")
+    await bookLibraryContract.deployed()
 
-    console.log('bookLibrary Contract address: ', bookLibraryContract.address);
-    console.log('Done!');
+    console.log("bookLibrary Contract address: ", bookLibraryContract.address)
+    console.log("Done!")
     await hre.run("verify:verify", {
         address: bookLibraryContract.address,
-        constructorArguments: [
-        ],
-    });
-    await hre.run('print', { message: "Verified." });
+        constructorArguments: [],
+    })
+    await hre.run("print", { message: "Verified." })
 }
 
-module.exports = deployLibraryContract;
+module.exports = deployLibraryContract
--- test/bookLibrary-test.js
+++ test/bookLibrary-test.js
@@ -1,68 +1,77 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
-
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 describe("BookLibrary", function () {
-    let dummyName = "Lorem Ipsum";
-    let bookCopies = 50;
-    let bookLibrary;
-    let bookLibraryFactory;
+    let dummyName = "Lorem Ipsum"
+    let bookCopies = 50
+    let bookLibrary
+    let bookLibraryFactory
     before(async () => {
-        bookLibraryFactory = await ethers.getContractFactory("Library");
-        bookLibrary = await bookLibraryFactory.deploy();
-        const [owner, addr1] = await ethers.getSigners();
-        await bookLibrary.deployed({ from: owner });
-    });
+        bookLibraryFactory = await ethers.getContractFactory("Library")
+        bookLibrary = await bookLibraryFactory.deploy()
+        const [owner, addr1] = await ethers.getSigners()
+        await bookLibrary.deployed({ from: owner })
+    })
 
     it("Should be reverted if other account is adding the book different from administrator", async function () {
-        const [owner, addr1] = await ethers.getSigners();
-        expect(bookLibrary.connect(addr1).addNewBook(dummyName, bookCopies)).to.be.revertedWith('Ownable: caller is not the owner');
-    });
+        const [owner, addr1] = await ethers.getSigners()
+        expect(
+            bookLibrary.connect(addr1).addNewBook(dummyName, bookCopies)
+        ).to.be.revertedWith("Ownable: caller is not the owner")
+    })
 
     it("Should throw if name of the book is not presented", async function () {
-        const [owner, addr1] = await ethers.getSigners();
-        const bookWithBlankName = "";
-        expect(bookLibrary.connect(owner).addNewBook(bookWithBlankName, bookCopies)).to.be.revertedWith("Name field must be not blank");
-    });
+        const [owner, addr1] = await ethers.getSigners()
+        const bookWithBlankName = ""
+        expect(
+            bookLibrary.connect(owner).addNewBook(bookWithBlankName, bookCopies)
+        ).to.be.revertedWith("Name field must be not blank")
+    })
 
     it("Should be created a new book from administrator", async function () {
-        const [owner, addr1] = await ethers.getSigners();
-        await bookLibrary.connect(owner).addNewBook(dummyName, bookCopies);
-        const getFirstBookDetail = await bookLibrary.getBookDetail(1);
-        expect(getFirstBookDetail[0]).to.be.equal(dummyName);
-    });
+        const [owner, addr1] = await ethers.getSigners()
+        await bookLibrary.connect(owner).addNewBook(dummyName, bookCopies)
+        const getFirstBookDetail = await bookLibrary.getBookDetail(1)
+        expect(getFirstBookDetail[0]).to.be.equal(dummyName)
+    })
 
     it("Should throw if the book is not borrowed but return action is initiated", async function () {
-        const [owner, addr1] = await ethers.getSigners();
-        expect(bookLibrary.connect(owner).returnBook(1)).to.be.revertedWith("Book is not borrowed.");
-    });
+        const [owner, addr1] = await ethers.getSigners()
+        expect(bookLibrary.connect(owner).returnBook(1)).to.be.revertedWith(
+            "Book is not borrowed."
+        )
+    })
 
     it("Should throw if book with same name is already available", async function () {
-        const [owner, addr1] = await ethers.getSigners();
-        expect(bookLibrary.connect(owner).addNewBook(dummyName, bookCopies)).to.be.revertedWith("Book is already available");
-    });
+        const [owner, addr1] = await ethers.getSigners()
+        expect(
+            bookLibrary.connect(owner).addNewBook(dummyName, bookCopies)
+        ).to.be.revertedWith("Book is already available")
+    })
 
     it("Should borrow a book and check if user is in borrow history", async function () {
-        const [owner, addr1, addr2] = await ethers.getSigners();
-        await bookLibrary.connect(addr1).borrowBook(1);
-        let borrowHistoryAddresses = await bookLibrary.getBookBorrowHistory(1);
-        expect(borrowHistoryAddresses).to.have.length(1);
-    });
+        const [owner, addr1, addr2] = await ethers.getSigners()
+        await bookLibrary.connect(addr1).borrowBook(1)
+        let borrowHistoryAddresses = await bookLibrary.getBookBorrowHistory(1)
+        expect(borrowHistoryAddresses).to.have.length(1)
+    })
 
     it("Admin adds book and users can't take it because no copies left", async function () {
-        const [owner, addr1, addr2] = await ethers.getSigners();
-        await bookLibrary.connect(owner).addNewBook("The only one", 1);
-        expect(bookLibrary.connect(addr1).borrowBook(2));
-        expect(bookLibrary.connect(addr2).borrowBook(2))
-            .to.be.revertedWithCustomError(bookLibrary, "NoCopiesLeft");
-    });
+        const [owner, addr1, addr2] = await ethers.getSigners()
+        await bookLibrary.connect(owner).addNewBook("The only one", 1)
+        expect(bookLibrary.connect(addr1).borrowBook(2))
+        expect(
+            bookLibrary.connect(addr2).borrowBook(2)
+        ).to.be.revertedWithCustomError(bookLibrary, "NoCopiesLeft")
+    })
 
     it("Should return a book and check if copies are one more", async function () {
-        const [owner, addr1, addr2] = await ethers.getSigners();
-        const oldCopiesCount = await bookLibrary.getBookDetail(1);
-        await bookLibrary.connect(addr1).returnBook(1);
-        const newCopiesCount = await bookLibrary.getBookDetail(1);
-        expect(parseInt(newCopiesCount[1])).to.be.equal(parseInt(oldCopiesCount[1]) + 1);
-    });
-
-});
+        const [owner, addr1, addr2] = await ethers.getSigners()
+        const oldCopiesCount = await bookLibrary.getBookDetail(1)
+        await bookLibrary.connect(addr1).returnBook(1)
+        const newCopiesCount = await bookLibrary.getBookDetail(1)
+        expect(parseInt(newCopiesCount[1])).to.be.equal(
+            parseInt(oldCopiesCount[1]) + 1
+        )
+    })
+})
